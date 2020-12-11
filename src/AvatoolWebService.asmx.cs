96d35e0e5f1bd0666d3ca4cd68bb6d0fdccec1f0
Chris Banwarth <APrettyCoolProgram@users.noreply.github.com>
2020-12-11 11:13:07 -0500

    Test #1

--- src/AvatoolWebService.asmx.cs
+++ src/AvatoolWebService.asmx.cs
@@ -1,14 +1,23 @@
-﻿// ===========================================================================================================  3:01 PM
-//    FILENAME: AvatoolWebService.asmx.cs
-//       BUILD: 20200123
-//     PROJECT: Avatool-Web-Service (https://github.com/spectrum-health-systems/Avatool-Web-Service)
-//     AUTHORS: development@aprettycoolprogram.com
-//   COPYRIGHT: Copyright 2019 A Pretty Cool Program
-//     LICENSE: Apache License, Version 2.0
-// ====================================================================================================================
-
-/* This code is heavily commented, the intention being that it's abundantly clear as to what it does, and how it works.
+﻿/* PROJECT: Avatool-Web-Service (https://github.com/spectrum-health-systems/Avatool-Web-Service)
+ *    FILE: Avatool_Web_Service.AvatoolWebService.asmx.cs
+ * UPDATED: 12-11-2020-9:18 AM
+ * LICENSE: Apache v2 (https://apache.org/licenses/LICENSE-2.0)
+ *          Copyright 2020 A Pretty Cool Program All rights reserved
  */
+
+/* Version 1.2-beta
+ */
+
+/* ==========================
+ * A NOTE ABOUT CODE COMMENTS
+ * ==========================
+ * The MyAvatarWebService (MAWS) source is heavily commented. This goes against best practice, but since other
+ * organizations may use it, I want to make it abundantly clear as to what MAWS does, and how it works. If you fork MAWS
+ * for your own development, please do not remove the original comments (and add nice, detailed comments of your own!).
+ *
+ * If you are viewing the development branch code, you will also see TODO comments.
+ */
+
 using System;
 using System.ComponentModel;
 using System.Web.Services;
@@ -16,58 +25,115 @@ using NTST.ScriptLinkService.Objects;
 
 namespace Avatool_Web_Service
 {
-    /// <summary>
-    /// Summary description for AvatoolWebService
-    /// </summary>
-    [WebService(Namespace         = "http://tempuri.org/")]
+    /// <summary>Summary description for MAWS</summary>
+    /// <remarks>This information is required.</remarks>
+    [WebService(Namespace = "http://tempuri.org/")]
     [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
     [ToolboxItem(false)]
 
-    // To allow this Web Service to be called from script, using ASP.NET AJAX, uncomment the following line.
-    // [System.Web.Script.Services.ScriptService]
     public class AvatoolWebService : WebService
     {
-        /// <summary>
-        /// Returns the Avatool Web Service version number.
-        /// </summary>
-        /// <returns>The Avatool Web Service version number</returns>
+        /// <summary>Returns the MAWS version number.</summary>
+        /// <returns>The MAWS version number.</returns>
+        /// <remarks>This method is required by myAvatar. Do not remove it!</remarks>
         [WebMethod]
-        public string GetVersion()
-        {
-            /* This method is required by myAvatar.
-             */
-            return "VERSION 1.1";
-        }
+        public string GetVersion() => "VERSION 1.2";
 
-        /// <summary>
-        /// Performs an Avatool Web Service "action".
-        /// </summary>
-        /// <param name="sentOptionObject">The OptionObject2 object from myAvatar.</param>
-        /// <param name="action">The action you want the Web Service to perform.</param>
-        /// <returns>A completed OptionObject.</returns>
+        /// <summary>Performs an MAWS "action".</summary>
+        /// <param name="sentOptionObject">The OptionObject2 object sent from myAvatar.</param>
+        /// <param name="action">The MAWS action perform.</param>
+        /// <returns>A completed OptionObject that MAWS will return to myAvatar.</returns>
+        /// <remarks>This method is required by myAvatar. Do not remove it!</remarks>
         [WebMethod]
         public OptionObject2 RunScript(OptionObject2 sentOptionObject, string action)
         {
-            /* When you create an Avatool Web Service ScriptLink event in myAvatar, you need to pass an action that the
-             * web service will perform. This action is the defined in the "parameter" field for the "Available Script"
-             * that you have selected in the Form Designer. Each action is handled by an individual method.
+            /* The main function of MAWS is to perform an "action" (a MAWS method call) using data that is recieved from
+             * myAvatar (an "OptionObject").
              *
-             * The action must be one of the case statements in the block of code below. The official Avatool Web
-             * Service release has the following potential actions:
+             * MAWS currently supports the following actions:
              *
-             *  - VerifyInpatientAdmissionDate : Verify that the Inpatient Admission Date is the same as the system
-             *                                   date.
+             *      VerifyInpatientAdmissionDate: Verify that the Inpatient Admission Date is the same as the system
+             *                                    current date.
+             *
+             * Each action has a MAWS method with the same name. For example, the "VerifyInpatientAdmissionDate" action
+             * is performed by the "MAWS.VerifyInpatientAdmissionDate()" method.
+             *
+             * To perform a MAWS action, you'll need to create a ScriptLink event in myAvatar that passes both an action
+             * request and an OptionObject to MAWS. The action is defined in the ScriptLink "Script Parameter" field,
+             * while MAWS is defined in the "Available Scripts" field.
+             *
+             * For detailed information and instructions about using Scriptlink and MAWS with myAvatar, please see the
+             * MAWS documentation here:
+             *
+             *      [URL]
              */
-            switch (action)
+
+            /* This switch statement will call the appropriate "action" method call. If the requested action is not one
+             * of the supported methods, the OptionObject is returned without any changes being made.
+             */
+            switch(action)
             {
+                case "CheckSubscriberPolicyNumber":
+                    return CheckSubscriberPolicyNumber(sentOptionObject);
+
                 case "VerifyInpatientAdmissionDate":
-                    return VerifyInpatientAdmissionDate(sentOptionObject);                                             // TODO Define the OptionObject here, and have a single return statement.
+                    return VerifyInpatientAdmissionDate(sentOptionObject);
 
                 default:
-                    return sentOptionObject;                                                                           // TODO Define the OptionObject here, and have a single return statement.
+                    return sentOptionObject;
             }
 
-            return sentOptionObject;
+            /* TODO - The OptionObject should be defined outside of the switch statement, assigned a value in the case
+             * statements, and passed back with a single return statement.
+             */
+            //return sentOptionObject;
+        }
+
+        public static OptionObject2 CheckSubscriberPolicyNumber(OptionObject2 sentOptionObject)
+        {
+            //Define the field we are looking for
+            //Init a placeholder
+            const string subscriberPolicyNumber      = "263";
+            var          subscriberPolicyNumberValue = "";
+
+            //Create a return box
+            //var returnOptionObject = new OptionObject2();
+
+            var returnOptionObject = sentOptionObject;
+
+
+            // Loop through forms
+            foreach(var form in returnOptionObject.Forms)
+            {
+                // Loop through form fields
+                foreach(var field in form.CurrentRow.Fields)
+                {
+                    // Do something with certian fields.
+                    switch(field.FieldNumber)
+                    {
+                        case subscriberPolicyNumber:
+                            field.FieldValue += "TEST";
+                            //field.FieldValue.Trim();
+                            //subscriberPolicyNumberValue = field.FieldValue;
+                            break;
+
+                        default:
+                            break;
+                    }
+                }
+            }
+
+            //Ask what to do?
+
+
+            //Trim()
+            //var finalValue = subscriberPolicyNumberValue.Trim();
+
+
+            //Put the trimmed data into the box
+
+            //Complete the box and return.
+            return CompleteOptionObject(sentOptionObject, returnOptionObject, true, false);
         }
 
         /// <summary>
@@ -105,16 +171,16 @@ namespace Avatool_Web_Service
 
             /* We'll start by looping through each of the forms passed via the OptionObject.
              */
-            foreach (var form in sentOptionObject.Forms)
+            foreach(var form in sentOptionObject.Forms)
             {
                 /* And for each of those forms, we'll look at each field.
                  */
-                foreach (var field in form.CurrentRow.Fields)
+                foreach(var field in form.CurrentRow.Fields)
                 {
                     /* If the field we are looking at is either "Type of Admission" or "Pre-Admit to Admission Date",
                      * we'll do something special.
                      */
-                    switch (field.FieldNumber)
+                    switch(field.FieldNumber)
                     {
                         case typeOfAdmissionField:
                             typeOfAdmission = int.Parse(field.FieldValue);                                             // TODO Convert.ToInt()?
@@ -150,9 +216,9 @@ namespace Avatool_Web_Service
             var errorMessageBody = string.Empty;
             var errorMessageCode = 0;
 
-            if (typeOfAdmission == preAdmission)
+            if(typeOfAdmission == preAdmission)
             {
-                if (preAdmitToAdmissionDate != systemDate)
+                if(preAdmitToAdmissionDate != systemDate)
                 {
                     errorMessageBody = "WARNING\nThe Pre-Admission date does not match today's date!";
                     errorMessageCode = 1;
@@ -163,7 +229,7 @@ namespace Avatool_Web_Service
 
             /* If there is a valid error code, add the error message info to the return object.
              */
-            if (errorMessageCode != 0)
+            if(errorMessageCode != 0)
             {
                 returnOptionObject.ErrorCode = errorMessageCode;
                 returnOptionObject.ErrorMesg = errorMessageBody;
@@ -221,15 +287,15 @@ namespace Avatool_Web_Service
             // Since it is recommended that these be explicitly set prior to returning the OptionObject2, they should
             // always be set by passing "true" as the value for the "recommended" argument. The if statement does its
             // best job to catch any invalid argument values.
-            if (recommended)
+            if(recommended)
             {
                 completedOptionObject.EpisodeNumber = sentOptionObject.EpisodeNumber;
                 completedOptionObject.OptionStaffId = sentOptionObject.OptionStaffId;
-                completedOptionObject.OptionUserId  = sentOptionObject.OptionUserId;
+                completedOptionObject.OptionUserId = sentOptionObject.OptionUserId;
 
                 // If the returnOptionObject has data, use that to complete the completedOptionObject. Otherwise, use
                 // the data that exists in the sentOptionObject.
-                if (returnOptionObject.ErrorCode >= 1)
+                if(returnOptionObject.ErrorCode >= 1)
                 {
                     completedOptionObject.ErrorCode = returnOptionObject.ErrorCode;
                     completedOptionObject.ErrorMesg = returnOptionObject.ErrorMesg;
@@ -245,7 +311,7 @@ namespace Avatool_Web_Service
             // setting them by passing "false" as the value for the "recommended" argument. Generally, if these fields
             // contain data when returned to myAvatar, this script will fail. The if statement does its  best job to
             // catch any invalid argument values.
-            if (notRecommended)
+            if(notRecommended)
             {
                 completedOptionObject.Forms = sentOptionObject.Forms;
             }
--- src/Resources/Dev/avatar-web-service-development-notes.md
+++ src/Resources/Dev/avatar-web-service-development-notes.md
@@ -1,6 +1,7 @@
 ﻿# AVATOOL WEB SERVICE DEVELOPMENT NOTES
 ## ROADMAP
 #### v2.0.0.0
+* Rename to "MyAvatoolWebService" (MAWS)
 * Split actions into seperate classes
 * Split returning the OO2 into a seperate class
 * Add Appointment Scheduler code
